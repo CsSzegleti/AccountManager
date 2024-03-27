f4f7b9a720118630b3b9d6854687192c38f13af7
CsSzegleti <szegleti_csongor@4dsoft.hu>
2024-03-27 13:08:21 +0100

    use criteria query instead of query string

--- src/main/kotlin/hu/cry0/account/persistence/AccountNumberGenerator.kt
+++ src/main/kotlin/hu/cry0/account/persistence/AccountNumberGenerator.kt
@@ -2,28 +2,28 @@ package hu.cry0.account.persistence
 
 import hu.cry0.account.ApplicationContextProvider
 import hu.cry0.account.config.BankProperty
+import hu.cry0.account.persistence.entity.AccountEntity
 import hu.cry0.account.persistence.exception.BankPrefixNotSetException
+import hu.cry0.account.persistence.exception.NoActiveSessionException
 import org.hibernate.engine.spi.SharedSessionContractImplementor
 import org.hibernate.id.IdentifierGenerator
 
 class AccountNumberGenerator : IdentifierGenerator {
 
     companion object {
-        private const val ACCOUNT_NUMBER_COUNT_QUERY = "SELECT COUNT(*) where account_number = %d"
+        private const val ACCOUNT_NUMBER_PROPERTY = "account_number"
     }
 
     private var bankProperty: BankProperty? = ApplicationContextProvider.bankProperty
 
     override fun generate(session: SharedSessionContractImplementor?, `object`: Any?): Any {
+        session?: throw NoActiveSessionException("error.no.active.session")
+
         var generatedNumber: Long
-        var isUnique: Boolean
 
         do {
             generatedNumber = generateNumber()
-            isUnique =
-                session?.createQuery(String.format(ACCOUNT_NUMBER_COUNT_QUERY, generateNumber()))?.stream()?.findFirst()
-                    ?.get() == 0
-        } while (!isUnique)
+        } while (isAccountNumberUnique(session, generatedNumber))
 
         return generatedNumber
     }
@@ -38,6 +38,17 @@ class AccountNumberGenerator : IdentifierGenerator {
                 toString()
             }.toLong()
         } ?: throw BankPrefixNotSetException("error.bank.prefix.notset")
+    }
+
+    private fun isAccountNumberUnique(session: SharedSessionContractImplementor, accountNumber: Long): Boolean {
+        val criteriaBuilder = session.criteriaBuilder
+        val criteriaQuery = criteriaBuilder.createQuery(Long::class.java)
+        val root = criteriaQuery.from(AccountEntity::class.java)
+
+        criteriaQuery.select(criteriaBuilder.count(root)).where(criteriaBuilder.equal(root?.get<Long>(ACCOUNT_NUMBER_PROPERTY), accountNumber))
+        val query = session.createQuery(criteriaQuery)
+
+        return query.singleResult > 0
     }
 
 }
