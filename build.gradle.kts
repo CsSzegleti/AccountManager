72ed829f6b7ef5248601a8e59cfbbeaeecdb9333
CsSzegleti <szegleti_csongor@4dsoft.hu>
2024-03-27 18:13:52 +0100

    add swagger dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.modelmapper:modelmapper:3.2.0")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 }
 
 tasks.withType<KotlinCompile> {
