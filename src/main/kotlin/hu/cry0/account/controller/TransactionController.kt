51a465f8fcae1e13f702981053b97a0124b0d23a
CsSzegleti <szegleti_csongor@4dsoft.hu>
2024-03-27 18:37:53 +0100

    add swagger to TransactionController

--- src/main/kotlin/hu/cry0/account/controller/TransactionController.kt
+++ src/main/kotlin/hu/cry0/account/controller/TransactionController.kt
@@ -1,12 +1,19 @@
 package hu.cry0.account.controller
 
+import hu.cry0.account.controller.error.ApiError
 import hu.cry0.account.model.Transaction
 import hu.cry0.account.model.validator.AccountActive
 import hu.cry0.account.service.TransactionService
-import jakarta.validation.Valid
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -23,29 +30,133 @@ import java.util.UUID
 @RequestMapping(value = ["/api/v1/account/{accountNumber}/transaction"])
 class TransactionController(private val transactionService: TransactionService) {
 
+    @Operation(
+        summary = "Get all transactions.",
+        description = "Get all transactions of an account",
+    )
+    @ApiResponses(
+        value = [ApiResponse(
+            description = "Successful operation", responseCode = "200", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                array = ArraySchema(schema = Schema(implementation = Transaction::class)),
+            )]
+        ), ApiResponse(
+            description = "Bad request", responseCode = "400", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Internal server error", responseCode = "500", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        )]
+    )
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Validated
     fun getAllByAccountNumber(
-        @PathVariable @Valid @AccountActive accountNumber: Long?
-    ) = ResponseEntity.ok(transactionService.getAllByAccountNumber(accountNumber!!))
+        @PathVariable @AccountActive accountNumber: Long
+    ) = ResponseEntity.ok(transactionService.getAllByAccountNumber(accountNumber))
 
+    @Operation(
+        summary = "Get an account's transaction by id",
+        description = "Get an account's transaction by id",
+    )
+    @ApiResponses(
+        value = [ApiResponse(
+            description = "Successful operation", responseCode = "200", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = Transaction::class),
+            )]
+        ), ApiResponse(
+            description = "Bad request", responseCode = "400", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Resource not found", responseCode = "404", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Internal server error", responseCode = "500", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        )]
+    )
     @GetMapping(path = ["/{transactionId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Validated
     fun getById(
-        @PathVariable @Valid @AccountActive accountNumber: Long,
+        @PathVariable @AccountActive accountNumber: Long,
         @PathVariable transactionId: UUID,
     ) = ResponseEntity.ok(transactionService.getById(transactionId, accountNumber))
 
+    @Operation(
+        summary = "Delete transaction.",
+        description = "Delete transaction.",
+    )
+    @ApiResponses(
+        value = [ApiResponse(
+            description = "Successful operation", responseCode = "204"
+        ), ApiResponse(
+            description = "Bad request", responseCode = "400", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Resource not found", responseCode = "404", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Internal server error", responseCode = "500", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        )]
+    )
     @DeleteMapping(path = ["/{transactionId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Validated
     fun deleteById(
-        @PathVariable @Valid @AccountActive accountNumber: Long,
+        @PathVariable @AccountActive accountNumber: Long,
         @PathVariable transactionId: UUID,
     ): ResponseEntity<*> {
         transactionService.deleteById(transactionId)
         return ResponseEntity.status(204).build<HttpStatus>()
     }
 
+    @Operation(
+        summary = "Add transaction to an account.",
+        description = "Add transaction to an account.",
+    )
+    @ApiResponses(
+        value = [ApiResponse(
+            description = "Successful operation", responseCode = "201", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = Transaction::class),
+            )]
+        ), ApiResponse(
+            description = "Bad request", responseCode = "400", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Resource not found", responseCode = "404", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Internal server error", responseCode = "500", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        )]
+    )
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @Validated
     fun addTransaction(
-        @PathVariable @Valid @AccountActive accountNumber: Long,
+        @PathVariable @AccountActive accountNumber: Long,
         @RequestBody transaction: Transaction,
     ): ResponseEntity<Transaction> {
         val saveResult = transactionService.addTransaction(transaction)
@@ -57,13 +168,41 @@ class TransactionController(private val transactionService: TransactionService) 
         return ResponseEntity.created(location).body(saveResult)
     }
 
+    @Operation(
+        summary = "Modify transaction on an account.",
+        description = "Modify transaction of an account.",
+    )
+    @ApiResponses(
+        value = [ApiResponse(
+            description = "Successful operation", responseCode = "200", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = Transaction::class),
+            )]
+        ), ApiResponse(
+            description = "Bad request", responseCode = "400", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Resource not found", responseCode = "404", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        ), ApiResponse(
+            description = "Internal server error", responseCode = "500", content = [Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = Schema(implementation = ApiError::class),
+            )]
+        )]
+    )
     @PutMapping(
         path = ["/{transactionId}"],
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
+    @Validated
     fun updateTransaction(
-        @PathVariable @Valid @AccountActive accountNumber: Long,
+        @PathVariable @AccountActive accountNumber: Long,
         @PathVariable transactionId: UUID,
         @RequestBody transaction: Transaction,
     ): ResponseEntity<Transaction> {
