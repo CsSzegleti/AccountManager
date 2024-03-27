8f8a070824daf08b02f95747839e3b7cc3e0c023
CsSzegleti <szegleti_csongor@4dsoft.hu>
2024-03-27 18:12:50 +0100

    rewrite validator

--- src/main/kotlin/hu/cry0/account/model/validator/ActiveAccountValidator.kt
+++ src/main/kotlin/hu/cry0/account/model/validator/ActiveAccountValidator.kt
@@ -1,16 +1,17 @@
 package hu.cry0.account.model.validator
 
-import hu.cry0.account.persistence.repository.AccountRepository
+import hu.cry0.account.model.AccountStatus
+import hu.cry0.account.service.AccountService
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 
 class ActiveAccountValidator(
-    private val accountRepository: AccountRepository
+    private val accountService: AccountService
 ) : ConstraintValidator<AccountActive, Long> {
 
     override fun isValid(accountNumber: Long?, validationContext: ConstraintValidatorContext?): Boolean {
         return accountNumber?.let {
-            accountRepository.existsById(accountNumber)
+            accountService.getAccountByNumber(accountNumber).status == AccountStatus.ACTIVE
         } ?: false
     }
 }
