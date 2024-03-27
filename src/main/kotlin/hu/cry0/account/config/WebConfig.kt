edeec07ffa9cc977052c4ced4dab4c8f3ccd70c6
CsSzegleti <szegleti_csongor@4dsoft.hu>
2024-03-27 18:13:32 +0100

    allow all other endpoints cross-origin

--- src/main/kotlin/hu/cry0/account/config/WebConfig.kt
+++ src/main/kotlin/hu/cry0/account/config/WebConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig(private val apiProperty: ApiProperty) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedOrigins("*")
         registry.addMapping("/api/v1/account/finalize").allowedOrigins(apiProperty.securityCheck.basePath)
     }
 }
