9b4c1a65144545ed35ef889c742cea52dac011f4
CsSzegleti <szegleti_csongor@4dsoft.hu>
2024-03-27 13:07:10 +0100

    static app context provider

---
+++ src/main/kotlin/hu/cry0/account/ApplicationContextProvider.kt
@@ -0,0 +1,25 @@
+package hu.cry0.account
+
+import hu.cry0.account.config.BankProperty
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import org.springframework.stereotype.Component
+
+@Component
+class ApplicationContextProvider : ApplicationContextAware {
+
+    companion object {
+
+        private var appContext: ApplicationContext? = null
+
+        val bankProperty: BankProperty?
+            get() = appContext?.getBean(BankProperty::class.java )
+
+        fun getApplicationContext() = appContext
+
+    }
+
+    override fun setApplicationContext(applicationContext: ApplicationContext) {
+        appContext = applicationContext
+    }
+}
