a4d48ced0a56891d11e8eb3b8b0d607a59d58539
CsSzegleti <szegleti_csongor@4dsoft.hu>
2024-03-28 09:53:27 +0100

    fix typos in sql

--- src/main/kotlin/hu/cry0/account/persistence/repository/AccountRepository.kt
+++ src/main/kotlin/hu/cry0/account/persistence/repository/AccountRepository.kt
@@ -9,9 +9,9 @@ interface AccountRepository : JpaRepository<AccountEntity, String> {
     @Query(
         """
         SELECT
-            ((SELECT COALESCE(SUM(t.amount), 0) FROM transaction t WHERE t.type='DEPOSIT' AND t.account_number=:accountNumber AND t.time_stamp <= CURRENT_TIMESTAMP)
+            ((SELECT COALESCE(SUM(t.amount), 0) FROM transaction t WHERE t.transaction_type='DEPOSIT' AND t.account_number=:accountNumber AND t.time_stamp <= CURRENT_TIMESTAMP)
                 -
-            (SELECT COALESCE(SUM(t.amount), 0) FROM transsaction t WHERE t.type='WITHDRAWAL' AND t.account_number=:accountNumber AND t.time_stamp <= CURRENT_TIMESTAMP))
+            (SELECT COALESCE(SUM(t.amount), 0) FROM transaction t WHERE t.transaction_type='WITHDRAWAL' AND t.account_number=:accountNumber AND t.time_stamp <= CURRENT_TIMESTAMP))
         """, nativeQuery = true
     )
     fun getAccountBalance(@Param("accountNumber") accountNumber: String): Long
