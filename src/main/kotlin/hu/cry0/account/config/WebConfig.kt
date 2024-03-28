777f2119a10110fdd7cd70d0fda6e11ad9844d55
CsSzegleti <szegleti_csongor@4dsoft.hu>
2024-03-28 09:54:17 +0100

    reorder cors config

--- src/main/kotlin/hu/cry0/account/config/WebConfig.kt
+++ src/main/kotlin/hu/cry0/account/config/WebConfig.kt
@@ -2,12 +2,14 @@ package hu.cry0.account.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableWebMvc
 class WebConfig(private val apiProperty: ApiProperty) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**").allowedOrigins("*")
         registry.addMapping("/api/v1/account/finalize").allowedOrigins(apiProperty.securityCheck.basePath)
+        registry.addMapping("/**").allowedOrigins("*")
     }
 }
